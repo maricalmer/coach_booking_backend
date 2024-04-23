56a94822ac42c949bedc815aedbd9ddcbb0622ab
Mari Calmer <superlassig@gmail.com>
2024-04-23 19:25:54 +0200

    feat: add validations on Coach and Slot models

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -1,2 +1,5 @@
 class Coach < ApplicationRecord
+  class Coach < ApplicationRecord
+    has_many :slots, dependent: :destroy
+  end
 end
--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,3 +1,4 @@
 class Slot < ApplicationRecord
   belongs_to :coach
+  validates :start_time, :end_time, presence: true
 end
